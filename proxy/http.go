32a428bce2c1047f43cb5f29719a8772d398ec7a
vsmk98 <vsaimk@gmail.com>
2020-11-30 15:06:29 +0800

    simplified the logic for processing RPC requests

--- proxy/http.go
+++ proxy/http.go
@@ -12,6 +12,11 @@ import (
 func makeHttpHandler(ps *ProxyServer) (http.HandlerFunc, error) {
 
 	return func(res http.ResponseWriter, req *http.Request) {
+		if err := ps.qrmNode.IsNodeBusy(); err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
 		body, err := ioutil.ReadAll(req.Body)
 		if err != nil {
 			const errMsg = "Reading request failed"
@@ -23,23 +28,12 @@ func makeHttpHandler(ps *ProxyServer) (http.HandlerFunc, error) {
 		// you can reassign the body if you need to parse it as multipart
 		req.Body = ioutil.NopCloser(bytes.NewReader(body))
 
-		if req.RequestURI != "/partyinfo" && req.RequestURI != "/partyinfo/validate" {
-			logRequestPayload(req, ps.proxyCfg.Name, ps.proxyCfg.UpstreamAddr, string(body))
-		}
-
-		// TODO move request URIs to be excluded to config
 		if req.RequestURI != "/partyinfo" && req.RequestURI != "/upcheck" && req.RequestURI != "/partyinfo/validate" {
+			logRequestPayload(req, ps.proxyCfg.Name, ps.proxyCfg.UpstreamAddr, string(body))
 			ps.qrmNode.ResetInactiveTime()
-		}
 
-		log.Info("httpHandler - request", "path", req.RequestURI)
+			log.Info("httpHandler - request", "path", req.RequestURI)
 
-		if err := ps.qrmNode.IsNodeBusy(); err != nil {
-			http.Error(res, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		if req.RequestURI != "/partyinfo" {
 			if ps.qrmNode.PrepareNode() {
 				log.Debug("httpHandler - prepared to accept request")
 			} else {
@@ -47,12 +41,13 @@ func makeHttpHandler(ps *ProxyServer) (http.HandlerFunc, error) {
 				http.Error(res, ErrNodeNotReady.Error(), http.StatusInternalServerError)
 				return
 			}
-		}
 
-		if err := HandlePrivateTx(body, ps); err != nil {
-			log.Error("httpHandler - handling pvt tx failed", "err", err)
-			http.Error(res, ErrParticipantsDown.Error(), http.StatusInternalServerError)
-			return
+			if err := HandlePrivateTx(body, ps); err != nil {
+				log.Error("httpHandler - handling pvt tx failed", "err", err)
+				http.Error(res, ErrParticipantsDown.Error(), http.StatusInternalServerError)
+				return
+			}
+
 		}
 
 		// Forward request to original request
