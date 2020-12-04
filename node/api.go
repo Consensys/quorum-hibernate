81eaa4ebbde34121903a34ea94192aaccaa55ae6
chris <chrishounsom@icloud.com>
2020-12-04 16:17:33 +0000

    Fix node.IsNodeUp API bug & return false instead of err if nodes are down * Bug always returned false even if nodes were up

--- node/api.go
+++ node/api.go
@@ -1,14 +1,11 @@
 package node
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/ConsenSysQuorum/node-manager/core/types"
-
-	"github.com/ConsenSysQuorum/node-manager/nodeman"
-
 	"github.com/ConsenSysQuorum/node-manager/log"
+	"github.com/ConsenSysQuorum/node-manager/nodeman"
 )
 
 type NodeRPCAPIs struct {
@@ -27,24 +24,21 @@ func NewNodeRPCAPIs(qn *NodeControl) *NodeRPCAPIs {
 	return &NodeRPCAPIs{qn: qn}
 }
 
-// IsClientUp checks if the node is up and returns the node's up status
-func (n *NodeRPCAPIs) IsNodeUp(req *http.Request, from *string, reply *NodeUpReply) error {
-	log.Debug("IsClientUp - rpc call isNodeUp", "from", *from)
-	ok := false
+// IsNodeUp checks if the node is up and returns the node's up status
+func (n *NodeRPCAPIs) IsNodeUp(_ *http.Request, from *string, reply *NodeUpReply) error {
+	log.Debug("IsNodeUp - rpc call isNodeUp", "from", *from)
 	if !n.qn.IsClientUp() {
-		log.Debug("IsClientUp - is node up failed")
-		ok = false
-		return errors.New("IsClientUp - is node up failed")
+		reply.Status = false
+		log.Debug("IsNodeUp - node not up")
+		return nil
 	}
-	*reply = NodeUpReply{
-		ok,
-	}
+	reply.Status = true
 	return nil
 }
 
 // PrepareForPrivateTx prepares this node for handling private transaction.
 // it returns status as true if preparing the node is successful else it returns status as false
-func (n *NodeRPCAPIs) PrepareForPrivateTx(req *http.Request, from *string, reply *PrivateTxPrepReply) error {
+func (n *NodeRPCAPIs) PrepareForPrivateTx(_ *http.Request, from *string, reply *PrivateTxPrepReply) error {
 	log.Debug("PrepareForPrivateTx - rpc call - request received to prepare node", "from", *from)
 	n.qn.ResetInactiveTime()
 	var status bool
@@ -68,8 +62,8 @@ func (n *NodeRPCAPIs) PrepareForPrivateTx(req *http.Request, from *string, reply
 	return nil
 }
 
-// ClientStatus returns current status of this node
-func (n *NodeRPCAPIs) NodeStatus(req *http.Request, from *string, reply *nodeman.NodeStatusInfo) error {
+// NodeStatus returns current status of this node
+func (n *NodeRPCAPIs) NodeStatus(_ *http.Request, from *string, reply *nodeman.NodeStatusInfo) error {
 	status := n.qn.GetNodeStatus()
 	inactiveTimeLimit := n.qn.config.BasicConfig.InactivityTime
 	curInactiveTimeCount := n.qn.im.GetInactivityTimeCount()
