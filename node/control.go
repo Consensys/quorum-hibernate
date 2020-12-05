4ff5942a5ffe4ae597fb6a77af489164a586890a
chris <chrishounsom@icloud.com>
2020-12-05 11:16:50 +0000

    Lock mutex when getting clientstatus

--- node/control.go
+++ node/control.go
@@ -49,6 +49,8 @@ type NodeControl struct {
 }
 
 func (n *NodeControl) ClientStatus() types.ClientStatus {
+	n.clntStatusMux.Lock()
+	defer n.clntStatusMux.Unlock()
 	return n.clientStatus
 }
 
@@ -301,7 +303,7 @@ func (n *NodeControl) WaitStopClient() bool {
 
 // TODO handle error if node failed to start
 func (n *NodeControl) PrepareClient() bool {
-	if n.clientStatus == types.Up {
+	if n.ClientStatus() == types.Up {
 		log.Debug("PrepareClient - node is up")
 		return true
 	} else {
@@ -316,7 +318,7 @@ func (n *NodeControl) StopClient() bool {
 	defer n.startStopMux.Unlock()
 	n.startStopMux.Lock()
 
-	if n.clientStatus == types.Down {
+	if n.ClientStatus() == types.Down {
 		log.Debug("StopClient - node is already down")
 		return true
 	}
@@ -400,7 +402,7 @@ func (n *NodeControl) stopProcesses() (bool, bool) {
 func (n *NodeControl) StartClient() bool {
 	defer n.startStopMux.Unlock()
 	n.startStopMux.Lock()
-	if n.clientStatus == types.Up {
+	if n.ClientStatus() == types.Up {
 		log.Debug("StartClient - node is already up")
 		return true
 	}
