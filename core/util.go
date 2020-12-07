ea492ef2743f1f5dd47535487f9538d85de3cc9b
chris <chrishounsom@icloud.com>
2020-12-07 12:02:15 +0000

    Flatten CallRPC & remove unnecessary table-driven tests

--- core/util.go
+++ core/util.go
@@ -34,7 +34,7 @@ func GetRandomRetryWaitTime(min int, max int) int {
 	return rand.Intn(max-min+1) + min
 }
 
-// CallRPC makes a rpc call to rpcUrl. It makes http post req with rpcTeq as body.
+// CallRPC makes a rpc call to rpcUrl. It makes http post req with rpcReq as body.
 // The returned JSON result is decoded into resData.
 // resData must be a pointer.
 func CallRPC(rpcUrl string, rpcReq []byte, resData interface{}) error {
@@ -45,25 +45,26 @@ func CallRPC(rpcUrl string, rpcReq []byte, resData interface{}) error {
 		return fmt.Errorf("CallRPC - creating request failed err=%v", err)
 	}
 	req.Header.Set("Content-Type", "application/json")
+
 	resp, err := client.Do(req)
 	if err != nil {
 		return fmt.Errorf("CallRPC - do req failed err=%v", err)
 	}
-
 	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusOK {
-		body, _ := ioutil.ReadAll(resp.Body)
-		log.Debug("CallRPC - response Body:", string(body))
-		err := json.Unmarshal(body, resData)
-		if err == nil {
-			log.Debug("CallRPC - response OK", "from", rpcUrl, "result", resData)
-		} else {
-			log.Error("CallRPC - response json decode failed", "err", err)
-			return err
-		}
-	} else {
+
+	if resp.StatusCode != http.StatusOK {
 		log.Error("CallRPC - response status failed, not OK", "status", resp.Status)
 		return fmt.Errorf("CallRPC - response status failed, not OK, status=%s", resp.Status)
 	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	log.Debug("CallRPC - response Body:", string(body))
+
+	if err := json.Unmarshal(body, resData); err != nil {
+		log.Error("CallRPC - response json decode failed", "err", err)
+		return err
+	}
+
+	log.Debug("CallRPC - response OK", "from", rpcUrl, "result", resData)
 	return nil
 }
--- core/util_test.go
+++ core/util_test.go
@@ -1,6 +1,11 @@
 package core
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -13,4 +18,181 @@ func Test_GetRandomRetryWaitTime(t *testing.T) {
 		}
 		c++
 	}
+}
+
+func TestCallRPC(t *testing.T) {
+	var (
+		rpcMethod = "app.DoSomething"
+		req       = fmt.Sprintf(`{"jsonrpc":2.0, "id":11, "method":"%v"}`, rpcMethod)
+		respCode  = 200
+		respBody  = `{"jsonrpc":"2.0","id":1,"result":{"someresponsedata": "val"}}`
+	)
+
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		require.Equal(t, req.Method, "POST")
+		require.Equal(t, req.Header["Content-Type"], []string{"application/json"})
+
+		type rpcRequest struct {
+			Method string
+		}
+
+		rpcReq := rpcRequest{}
+
+		err := json.NewDecoder(req.Body).Decode(&rpcReq)
+		require.NoError(t, err)
+		require.Equal(t, rpcMethod, rpcReq.Method)
+
+		w.WriteHeader(respCode)
+		_, err = w.Write([]byte(respBody))
+		require.NoError(t, err)
+	})
+
+	mockServer := httptest.NewServer(serverMux)
+
+	var got interface{}
+
+	err := CallRPC(mockServer.URL, []byte(req), &got)
+	require.NoError(t, err)
+
+	rpcResp := got.(map[string]interface{})
+	rpcResult := rpcResp["result"].(map[string]interface{})
+
+	require.Contains(t, rpcResult, "someresponsedata")
+}
+
+func TestCallRPC_HTTPError(t *testing.T) {
+	var (
+		rpcMethod = "app.DoSomething"
+		req       = fmt.Sprintf(`{"jsonrpc":2.0, "id":11, "method":"%v"}`, rpcMethod)
+	)
+
+	var tests = []struct {
+		name     string
+		respCode int
+		respBody string
+		wantErr  string
+	}{
+		{name: "clientError", respCode: 400, wantErr: "CallRPC - response status failed, not OK, status=400 Bad Request"},
+		{name: "serverError", respCode: 500, wantErr: "CallRPC - response status failed, not OK, status=500 Internal Server Error"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			serverMux := http.NewServeMux()
+			serverMux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+
+				require.Equal(t, req.Method, "POST")
+				require.Equal(t, req.Header["Content-Type"], []string{"application/json"})
+
+				type rpcRequest struct {
+					Method string
+				}
+
+				rpcReq := rpcRequest{}
+
+				err := json.NewDecoder(req.Body).Decode(&rpcReq)
+				require.NoError(t, err)
+				require.Equal(t, rpcMethod, rpcReq.Method)
+
+				w.WriteHeader(tt.respCode)
+				_, err = w.Write([]byte(tt.respBody))
+				require.NoError(t, err)
+			})
+
+			mockServer := httptest.NewServer(serverMux)
+
+			var resp interface{}
+
+			err := CallRPC(mockServer.URL, []byte(req), &resp)
+			require.EqualError(t, err, tt.wantErr)
+			require.Empty(t, resp)
+		})
+	}
+}
+
+func TestCallRPC_RpcError(t *testing.T) {
+	var (
+		rpcMethod = "app.DoSomething"
+		req       = fmt.Sprintf(`{"jsonrpc":2.0, "id":11, "method":"%v"}`, rpcMethod)
+		respCode  = 200
+		respBody  = `{"jsonrpc":"2.0","id":1,"error":{"code":100,"message":"someerrormessage", "data":{"field": "val"}}}`
+	)
+
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+
+		require.Equal(t, req.Method, "POST")
+		require.Equal(t, req.Header["Content-Type"], []string{"application/json"})
+
+		type rpcRequest struct {
+			Method string
+		}
+
+		rpcReq := rpcRequest{}
+
+		err := json.NewDecoder(req.Body).Decode(&rpcReq)
+		require.NoError(t, err)
+		require.Equal(t, rpcMethod, rpcReq.Method)
+
+		w.WriteHeader(respCode)
+		_, err = w.Write([]byte(respBody))
+		require.NoError(t, err)
+	})
+
+	mockServer := httptest.NewServer(serverMux)
+
+	var got interface{}
+
+	err := CallRPC(mockServer.URL, []byte(req), &got)
+	require.NoError(t, err)
+
+	rpcResp := got.(map[string]interface{})
+	rpcErr := rpcResp["error"].(map[string]interface{})
+	require.Equal(t, rpcErr["message"], "someerrormessage")
+
+	rpcErrData := rpcErr["data"].(map[string]interface{})
+	require.Contains(t, rpcErrData, "field")
+}
+
+func TestCallRPC_InvalidRespType(t *testing.T) {
+	var (
+		rpcMethod = "app.DoSomething"
+		req       = fmt.Sprintf(`{"jsonrpc":2.0, "id":11, "method":"%v"}`, rpcMethod)
+		respCode  = 200
+		respBody  = `{"jsonrpc":"2.0","id":1,"result":{"someresponsedata": "val"}}`
+	)
+
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+
+		require.Equal(t, req.Method, "POST")
+		require.Equal(t, req.Header["Content-Type"], []string{"application/json"})
+
+		type rpcRequest struct {
+			Method string
+		}
+
+		rpcReq := rpcRequest{}
+
+		err := json.NewDecoder(req.Body).Decode(&rpcReq)
+		require.NoError(t, err)
+		require.Equal(t, rpcMethod, rpcReq.Method)
+
+		w.WriteHeader(respCode)
+		_, err = w.Write([]byte(respBody))
+		require.NoError(t, err)
+	})
+
+	mockServer := httptest.NewServer(serverMux)
+
+	type invalid struct {
+		NotKnown int
+	}
+
+	var got invalid
+
+	err := CallRPC(mockServer.URL, []byte(req), &got)
+	require.NoError(t, err)
+	require.Empty(t, got)
 }
