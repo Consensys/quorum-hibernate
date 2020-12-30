90a6b21c9f1ce894f0a00cde324effb9169a5252
chris <chrishounsom@icloud.com>
2020-12-30 15:46:53 +0000

    Remove unused err type

--- config/common.go
+++ config/common.go
@@ -1,18 +1,8 @@
 package config
 
 import (
-	"fmt"
 	"net/url"
 )
-
-// namedValidationError provides additional context to an error, useful for providing context when there is a validation error with an element in an array
-type namedValidationError struct {
-	name, errMsg string
-}
-
-func (e namedValidationError) Error() string {
-	return fmt.Sprintf("name = %v: %v", e.name, e.errMsg)
-}
 
 func isValidUrl(addr string) error {
 	_, err := url.Parse(addr)
